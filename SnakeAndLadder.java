83bd320b3e9ec766afac00be3ae4acb14a4a66a5
prajavalpatki <prajavalpatki.com>
2022-03-12 19:51:15 +0530

    Check the player play or not

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,10 +4,15 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         int position = 0;
+        int play = 1;
+        int playornot = (int) Math.floor (Math.random() * 10) %2;
 
-        int randomCheck = (int) Math.floor (Math.random() * 10) % 6;
+        if (playornot == play) {
 
-        position = position + randomCheck;
+            int randomCheck = (int) Math.floor(Math.random() * 10) % 6;
+
+            position = position + randomCheck;
+        }
 
         System.out.println("position = " + position);
     }
