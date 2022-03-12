33b3d7922d1c237238c2c445ccafb36355d2d560
prajavalpatki <prajavalpatki.com>
2022-03-12 19:56:18 +0530

    Calculate the count to rich the position is 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,15 +5,21 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int position = 0;
         int play = 1;
-        int playornot = (int) Math.floor (Math.random() * 10) %2;
+        int count = 0;
 
-        if (playornot == play) {
+        for(int i = 1; position <= 100; i++ ) {
 
-            int randomCheck = (int) Math.floor(Math.random() * 10) % 6;
+            int playornot = (int) Math.floor(Math.random() * 10) % 2;
 
-            position = position + randomCheck;
+            if (playornot == play) {
+
+                int randomCheck = (int) Math.floor(Math.random() * 10) % 6;
+
+                position = position + randomCheck;
+            }
+            count++;
         }
 
-        System.out.println("position = " + position);
+        System.out.println("To reach the position " + position + " the count is = " + count);
     }
 }
