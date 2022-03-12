4d179f86578f9c4bb1cd4d7aac1399b8246cd38b
prajavalpatki <prajavalpatki.com>
2022-03-12 19:42:37 +0530

    Start positin 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("position=" +position);
+    }
+}
